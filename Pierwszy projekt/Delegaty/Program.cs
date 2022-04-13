a35d9d8c523897d431d59245c1418f10df1638ec
Łukasz Felisek <felisek1@wp.pl>
2022-04-13 09:20:58 +0200

    koniec lekcji

---
+++ Pierwszy projekt/Delegaty/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Delegaty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestDelegat testDelegat = new TestDelegat();
+            testDelegat.Test();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Pierwszy projekt/Delegaty/TestDelegat.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegaty
+{
+    class TestDelegat
+    {
+        delegate int OperacjaDoWykonaniaDelegate(int x, int y);
+
+        public void Test()
+        {
+            Console.WriteLine("Podaj pierwsza liczbe");
+            string xStr = Console.ReadLine();
+            int x = int.Parse(xStr);
+            Console.WriteLine("Podaj druga liczbe");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj operacje do wykonania");
+            char operacja = Console.ReadLine()[0];
+            int wynik;
+
+            //wersja pierwsza
+            switch (operacja)
+            {
+                case '+':
+                    wynik = Suma(x, y);
+                    break;
+                case '*':
+                    wynik = Iloczyn(x, y);
+                    break;
+                default:
+                    wynik = 0;
+                    break;
+            }
+            WyswieltWynikOperacji(wynik, operacja);
+
+            //wersja druga
+            OperacjaDoWykonaniaDelegate operacjaDoWykonaniaDelegate;
+            switch (operacja)
+            {
+                case '+':
+                    operacjaDoWykonaniaDelegate = Suma;
+                    break;
+                case '*':
+                    operacjaDoWykonaniaDelegate = Iloczyn;
+                    break;
+                default:
+                    operacjaDoWykonaniaDelegate = null;
+                    break;
+            }
+
+            if (operacjaDoWykonaniaDelegate != null)
+                wynik = operacjaDoWykonaniaDelegate(x, y);
+            else
+                wynik = 0;
+
+            WyswieltWynikOperacji(wynik, operacja);
+
+        }
+
+        private void WyswieltWynikOperacji(int wynik, char operacja)
+        {
+            Console.WriteLine("Wynik operacji "+ operacja + " to " + wynik);
+        }
+
+        private int Suma(int a, int b)
+        {
+            return a + b;
+        }
+
+        private int Iloczyn(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
