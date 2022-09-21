8c7d543765b15e69a7a57091d8ecf36e621432be
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-09-21 09:03:33 +0200

    koniec lekcji

---
+++ Pierwszy projekt/FirstWinFormsApp/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace FirstWinFormsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(287, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 114);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Przycisk";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 457);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Pierwszy program okienkowy";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
