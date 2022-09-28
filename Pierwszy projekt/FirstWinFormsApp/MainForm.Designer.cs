4dfcddda6d431d13bdacf02b27ef6a2b64efcb55
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-09-28 09:03:14 +0200

    fghcfg

--- Pierwszy projekt/FirstWinFormsApp/MainForm.Designer.cs
+++ Pierwszy projekt/FirstWinFormsApp/MainForm.Designer.cs
@@ -30,24 +30,35 @@ namespace FirstWinFormsApp
         private void InitializeComponent()
         {
             this.buttonFirst = new System.Windows.Forms.Button();
+            this.buttonHello = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonFirst
             // 
             this.buttonFirst.Font = new System.Drawing.Font("Segoe UI", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
-            this.buttonFirst.Location = new System.Drawing.Point(279, 116);
+            this.buttonFirst.Location = new System.Drawing.Point(67, 193);
             this.buttonFirst.Name = "buttonFirst";
             this.buttonFirst.Size = new System.Drawing.Size(216, 114);
             this.buttonFirst.TabIndex = 0;
             this.buttonFirst.Text = "Przycisk";
             this.buttonFirst.UseVisualStyleBackColor = true;
-            this.buttonFirst.Click += buttonFirst_Click;
+            this.buttonFirst.Click += new System.EventHandler(this.buttonFirst_Click);
+            // 
+            // buttonHello
+            // 
+            this.buttonHello.Location = new System.Drawing.Point(463, 204);
+            this.buttonHello.Name = "buttonHello";
+            this.buttonHello.Size = new System.Drawing.Size(281, 103);
+            this.buttonHello.TabIndex = 1;
+            this.buttonHello.Text = "Hello";
+            this.buttonHello.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(859, 457);
+            this.Controls.Add(this.buttonHello);
             this.Controls.Add(this.buttonFirst);
             this.MaximizeBox = false;
             this.Name = "MainForm";
@@ -59,6 +70,7 @@ namespace FirstWinFormsApp
         #endregion
 
         private System.Windows.Forms.Button buttonFirst;
+        private System.Windows.Forms.Button buttonHello;
     }
 }
 
