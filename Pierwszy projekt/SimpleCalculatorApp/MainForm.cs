cad5b1bbbfd237ab1c9fbd54673e436f0a3fc5e8
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-11-28 14:08:10 +0100

    wdf

--- Pierwszy projekt/SimpleCalculatorApp/MainForm.cs
+++ Pierwszy projekt/SimpleCalculatorApp/MainForm.cs
@@ -17,6 +17,21 @@ namespace SimpleCalculatorApp
             InitializeComponent();
         }
 
+        private void buttonAdd_Click_TMP(object sender, EventArgs e)
+        {
+            int firstNumber;
+            int secondNumber;
+            if (Validate(textBoxFirsNumber.Text, textBoxSecondNumber.Text,
+                out firstNumber, out secondNumber))
+            {
+                Calculate('+', firstNumber, secondNumber);
+            }
+            else
+            {
+                ShowErrorMessage();
+            }
+        }
+
         private void buttonAdd_Click(object sender, EventArgs e)
         {
             int firstNumber;
@@ -46,6 +61,46 @@ namespace SimpleCalculatorApp
             else
             {
                 labelResult.Text = "Podano nieprawidłowe dane";
+            }
+        }
+
+        private void ShowErrorMessage()
+        {
+            labelResult.Text = "Podano nieprawidłowe dane";
+        }
+
+        private bool Validate(string firstStrNumber, string secondStrNumber,
+                              out int firstNumber, out int secondNumber)
+        {
+            //firstNumber = 0;
+            secondNumber = 0;
+            return int.TryParse(firstStrNumber, out firstNumber)
+                && int.TryParse(secondStrNumber, out secondNumber);
+        }
+
+        private void Calculate(char operatorToDo, int firstNumber, int secondNumber)
+        {
+            int result;
+            switch (operatorToDo)
+            {
+                case '+':
+                    result = firstNumber + secondNumber;
+                    labelResult.Text = "Wynik operacji dodawania: " + result;
+                    break;
+                case '-':
+                    result = firstNumber - secondNumber;
+                    labelResult.Text = "Wynik operacji odejmowania: " + result;
+                    break;
+                case '*':
+                    result = firstNumber * secondNumber;
+                    labelResult.Text = "Wynik operacji mnożenia: " + result;
+                    break;
+                case '/':
+                    result = firstNumber / secondNumber;
+                    labelResult.Text = "Wynik operacji dzielenia: " + result;
+                    break;
+                default:
+                    break;
             }
         }
     }
