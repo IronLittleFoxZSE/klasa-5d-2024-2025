35a89bc519efa9f483fc10aa5288e4f11501567b
IronLittleFox <lukasz.felisek@gmail.com>
2023-09-21 10:13:25 +0200

    koniec lekcji

---
+++ Pierwszy projekt/ComboboxWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComboboxWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public ObservableCollection<string> ItemsList { get; set; }
+        public string NewPosition { get; set; }
+
+        #region informowanie bindowania
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ItemsList = new ObservableCollection<string>();
+            OnPropertyChanged(nameof(ItemsList));
+
+            ItemsList.Add("Pozycja bindowania 1");
+            ItemsList.Add("Pozycja bindowania 2");
+            ItemsList.Add("Pozycja bindowania 3");
+        }
+
+        private void ButtonNewComboboxPosition_Click(object sender, RoutedEventArgs e)
+        {
+            ItemsList.Add(NewPosition);
+            OnPropertyChanged(nameof(ItemsList));
+        }
+    }
+}
