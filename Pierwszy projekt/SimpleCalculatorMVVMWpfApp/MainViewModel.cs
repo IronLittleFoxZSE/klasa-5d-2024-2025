8c8543d6db2ed9a4832020c557efe14e5b0ac0fb
IronLittleFox <lukasz.felisek@gmail.com>
2023-10-31 11:18:22 +0100

    koniec lekcji

---
+++ Pierwszy projekt/SimpleCalculatorMVVMWpfApp/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtilsWpf;
+
+namespace SimpleCalculatorMVVMWpfApp
+{
+    public class MainViewModel : ObserverVM
+    {
+        private int firsNumber;
+        public int FirstNumber
+        {
+            get { return firsNumber; }
+            set
+            {
+                firsNumber = value;
+                OnPropertyChanged(nameof(FirstNumber));
+            }
+        }
+
+        private int secondNumber;
+        public int SecondNumber
+        {
+            get { return secondNumber; }
+            set
+            {
+                secondNumber = value;
+                OnPropertyChanged(nameof(SecondNumber));
+            }
+        }
+
+        private int result;
+        public int Result
+        {
+            get { return result; }
+            set
+            {
+                result = value;
+                OnPropertyChanged(nameof(Result));
+            }
+        }
+    }
+}
---
+++ Pierwszy projekt/UtilsWpf/ObserverVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace UtilsWpf
+{
+    public class ObserverVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
