2e7cc6a4b0e9233bb6777b67e89e896d4b4efdd0
IronLittleFox <lukasz.felisek@gmail.com>
2023-12-05 12:05:17 +0100

    f

--- Pierwszy projekt/UtilsWpf/RelayCommand.cs
+++ Pierwszy projekt/UtilsWpf/RelayCommand.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace UtilsWpf
 {
-    internal class RelayCommand
+    public class RelayCommand<T> : ICommand
     {
+        private Action<T> execute;
+        private Func<T, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { }
+            remove { }
+        }
+
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute((T)parameter);
+        }
     }
 }
