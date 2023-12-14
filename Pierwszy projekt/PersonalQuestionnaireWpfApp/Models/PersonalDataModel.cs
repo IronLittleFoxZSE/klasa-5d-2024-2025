e0968a022de537cca5aff8d811be10f406fae73b
IronLittleFox <lukasz.felisek@gmail.com>
2023-12-14 13:47:51 +0100

    koniec lekcji

---
+++ Pierwszy projekt/PersonalQuestionnaireWpfApp/Models/PersonalDataModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalQuestionnaireWpfApp.Models
+{
+    public struct PersonalDataModel
+    {
+        public string name;
+        public bool isPizzaWithPineapple;
+    }
+}
---
+++ Pierwszy projekt/PersonalQuestionnaireWpfApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,68 @@
+﻿using PersonalQuestionnaireWpfApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UtilsWpf;
+
+namespace PersonalQuestionnaireWpfApp.ViewModels
+{
+    public class MainViewModel : ObserverVM
+    {
+        private PersonalDataModel personalDataModel = new PersonalDataModel();
+
+        public string Name
+        {
+            get { return personalDataModel.name; }
+            set
+            {
+                personalDataModel.name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public bool PizzaWithPineapple
+        {
+            get { return personalDataModel.isPizzaWithPineapple; }
+            set
+            {
+                personalDataModel.isPizzaWithPineapple = value;
+                OnPropertyChanged(nameof(PizzaWithPineapple));
+            }
+        }
+
+
+        private string questionnaireResult;
+        public string QuestionnaireResult
+        {
+            get { return questionnaireResult; }
+            set
+            {
+                questionnaireResult = value;
+                OnPropertyChanged(nameof(QuestionnaireResult));
+            }
+        }
+
+        private ICommand downloadDataCommand;
+        public ICommand DownloadDataComand
+        {
+            get
+            {
+                if (downloadDataCommand == null)
+                    downloadDataCommand = new RelayCommand<object>(
+                        o =>
+                        {
+                            string result = "";
+                            result += "Imię: " + Name + "\n";
+                            result += "Pizza z ananasem: " + (PizzaWithPineapple ? "Tak" : "Nie") + "\n";
+
+                            QuestionnaireResult = result;
+                        }
+                        );
+                return downloadDataCommand;
+            }
+        }
+    }
+}
