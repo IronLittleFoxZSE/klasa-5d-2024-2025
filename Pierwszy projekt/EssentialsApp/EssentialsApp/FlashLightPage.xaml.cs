bf10fe73a1ee6e81cb0129858516682a5a15e7ba
g <g@g.pl>
2024-04-09 10:09:04 +0200

    koniec lekcji

---
+++ Pierwszy projekt/EssentialsApp/EssentialsApp/FlashLightPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
+namespace EssentialsApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FlashLightPage : ContentPage
+    {
+        public FlashLightPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TurnOn_Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Flashlight.TurnOnAsync();
+            }
+            catch (Exception)
+            {
+
+                //throw;
+            }
+        }
+
+        private void TurnOf_Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Flashlight.TurnOffAsync();
+            }
+            catch (Exception)
+            {
+
+                //throw;
+            }
+        }
+    }
+}
---
+++ Pierwszy projekt/EssentialsApp/EssentialsApp/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EssentialsApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void FlashLight_Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new FlashLightPage());
+        }
+    }
+}
